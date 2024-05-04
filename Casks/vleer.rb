e25d41ff228348c814c209f1cdb8766626546c9c
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-05-04 11:40:07 +0200

    changed url

--- Casks/vleer.rb
+++ Casks/vleer.rb
@@ -1,11 +1,11 @@
-cask "vleer" do
-  version "1.0.0-pre.3"
+cask "vleer@beta" do
+  version "1.0.0-beta.3"
   sha256 "08adac24441a6ba9409209a2c4788ae8e8c9e25c04e5535d1184cf3024d7cf13"
 
   url "https://github.com/Vleerapp/Vleer/releases/download/v#{version}/Vleer_#{version}_aarch64.dmg"
   name "Vleer"
   desc "A music app from the future"
-  homepage "https://vleer.app"
+  homepage "https://github.com/Vleerapp/Vleer"
 
   app "Vleer.app"
 end
