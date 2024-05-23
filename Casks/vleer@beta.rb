54c1d93ee51342629c8a246d4e1f9a89be1144d9
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-05-23 12:07:50 +0200

    Update vleer@beta.rb

--- Casks/vleer@beta.rb
+++ Casks/vleer@beta.rb
@@ -1,8 +1,8 @@
-cask "vleer@beta" do
-  version "1.0.0-beta.4"
-  sha256 "738617c1ee8bbcded796643656b2c45ff5e698b6fc2527a5462f955070a35cb1"
+cask "vleer" do
+  version "0.1.0"
+  sha256 "a851d0ded4fd6db9823bbac30385b07ccc947969f375e0aa5c3f508cfc129e82"
 
-  url "https://github.com/Vleerapp/Vleer/releases/download/v#{version}/Vleer_#{version}_aarch64.dmg"
+  url "https://github.com/Vleerapp/Vleer/releases/download/v#{version}/Vleer_#{version}.dmg"
   name "Vleer"
   desc "Music app from the future"
   homepage "https://github.com/Vleerapp/Vleer"
