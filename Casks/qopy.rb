cd345dacb429e3cfd4cf99b7c0ac953017929dbe
PandaDEV <github.com@pandadev.net>
2025-02-19 11:39:47 +0100

    Update qopy.rb

--- Casks/qopy.rb
+++ Casks/qopy.rb
@@ -1,12 +1,12 @@
 cask "qopy" do
-  version "0.3.2"
+  version "0.3.3"
 
   if Hardware::CPU.intel?
     url "https://github.com/0pandadev/Qopy/releases/download/v#{version}/Qopy-#{version}_intel.dmg"
-    sha256 "1551e0396b2a360ccf96b486f9b8afd9629d3de31dec7bdff3d2483c520dd7f6"
+    sha256 "a94966fbe12c8ed62c359469fb00357fd2345efc31a8526c58fee9ff682e5e23"
   else
     url "https://github.com/0pandadev/Qopy/releases/download/v#{version}/Qopy-#{version}_silicon.dmg"
-    sha256 "e05f361c70c01da9cd32832b4cf675546321c189ebf44769b2c13c09bc01e3bf"
+    sha256 "9f2985260060d51bc2e8fb27d30eb098e6800c78934d376de06131d6eea0cc28"
   end
   
   name "Qopy"
